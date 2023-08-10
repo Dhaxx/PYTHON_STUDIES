78886539fa39df042b86293bc4e8ad73607074d6
Dhaxx <kaiopablo44@gmail.com>
2023-08-09 23:45:22 -0300

    Exercício Finalizado

---
+++ EXERCICIOS/numero_duplicado.py
@@ -0,0 +1,32 @@
+"""CRIE UMA FUNÇÃO QUE RETORNE O PRIMEIRO NÚMERO DUPLICADO."""
+
+lista_de_listas_de_inteiros = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
+
+def localiza_numero_duplicado(lista):
+    for row in lista:
+        print(row,end=' => ')
+        s1 = set()
+        for i, col in enumerate(row):
+            i += 1
+            if col not in s1:
+                s1.add(col)
+                print(-1) if i == 10 else None
+            elif col in s1:
+                print(col)
+                break
+            
+
+localiza_numero_duplicado(lista_de_listas_de_inteiros)
