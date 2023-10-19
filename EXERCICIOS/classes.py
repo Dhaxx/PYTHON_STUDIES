93ec1c56b5c339d3d90c5320f3ad0d1a9fa38a40
Dhaxx <kaiopablo44@gmail.com>
2023-10-18 23:15:33 -0300

    Concluindo mais exercícios de classe

--- EXERCICIOS/classes.py
+++ EXERCICIOS/classes.py
@@ -54,3 +54,73 @@
     b.Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
     c.Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe as medidades de um local. Depois,
       deve criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés necessárias para o local.'''
+
+# # Constantes 
+# AREA_PISO = 1 
+# LARGURA_RODAPE = 0.1
+
+# class Retangulo:
+#     def __init__(self, base, altura):
+#         self._base = base
+#         self._altura = altura
+
+#     @property
+#     def base(self):
+#         return self._base
+    
+#     @base.setter
+#     def base(self, value):
+#         self._base = value
+    
+#     @property
+#     def altura(self):
+#         return self._altura
+    
+#     @altura.setter
+#     def altura(self, value):
+#         self._altura = value
+    
+#     def area(self):
+#         return self._base * self._altura
+    
+#     def perimetro(self):
+#         return 2 * (self._base + self._altura)
+
+# base = float(input('Insira a medida da base: '))
+# altura = float(input('Insira a medida da altura: '))
+
+# retangulo = Retangulo(base, altura)
+# print(f'Base: {retangulo.base:.2f}')
+# print(f'Altura: {retangulo.altura:.2f}')
+
+# quantidade_de_pisos = retangulo.area() / AREA_PISO
+# quantidade_de_rodapes = retangulo.perimetro() * LARGURA_RODAPE
+
+# print(f'A quantidade de Pisos é: {quantidade_de_pisos:.2f}')
+# print(f'A quantidade de Rodapés é: {quantidade_de_rodapes:.2f}')
+
+'''Classe Pessoa: Crie uma classe que modele uma pessoa:
+    Atributos: nome, idade, peso e altura
+    Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.'''
+
+# class Pessoa:
+#     def __init__(self, nome, idade, peso, altura):
+#         self.nome = nome          
+#         self.idade = idade
+#         self.peso = float(peso)
+#         self.altura = altura
+
+#     def envelhecer(self):
+#         self.idade += 1
+
+#     def crescer(self):
+#         if self.idade <= 21:
+#             self.crescer += 0.5
+
+#     def engordar(self):
+#         self.peso += 1.0
+
+#     def emagrecer(self):
+#         self.peso -= 1.0
+
+''''''
